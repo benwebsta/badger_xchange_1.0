c56c2c7269c96109fc2691ae9d4c5db030c8477e
Ben Webster <bmwebster@wisc.edu>
2016-07-03 15:09:13 -0500

    uncommented admob_simple.js got ads to work again

--- www/js/admob_simple.js
+++ www/js/admob_simple.js
@@ -1,46 +1,46 @@
-// var admobid = {};
+var admobid = {};
 
-// // TODO: replace the following ad units with your own
-// if( /(android)/i.test(navigator.userAgent) ) { 
-//   admobid = { // for Android
-//     banner: 'ca-app-pub-8780946757864821/6248315999',
-//     interstitial: 'ca-app-pub-6869992474017983/1657046752'
-//   };
-// } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-//   admobid = { // for iOS
-//     banner: 'ca-app-pub-8780946757864821/6248315999',
-//     interstitial: 'ca-app-pub-6869992474017983/7563979554'
-//   };
-// } else {
-//   admobid = { // for Windows Phone
-//     banner: 'ca-app-pub-8780946757864821/6248315999',
-//     interstitial: 'ca-app-pub-6869992474017983/1355127956'
-//   };
-// }
+// TODO: replace the following ad units with your own
+if( /(android)/i.test(navigator.userAgent) ) { 
+  admobid = { // for Android
+    banner: 'ca-app-pub-8780946757864821/6248315999',
+    interstitial: 'ca-app-pub-6869992474017983/1657046752'
+  };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+  admobid = { // for iOS
+    banner: 'ca-app-pub-8780946757864821/6248315999',
+    interstitial: 'ca-app-pub-6869992474017983/7563979554'
+  };
+} else {
+  admobid = { // for Windows Phone
+    banner: 'ca-app-pub-8780946757864821/6248315999',
+    interstitial: 'ca-app-pub-6869992474017983/1355127956'
+  };
+}
 
-// function initApp() {
-//   if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+function initApp() {
+  if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
 
-//   // this will create a banner on startup
-//   AdMob.createBanner( {
-//     adId: admobid.banner,
-//     position: AdMob.AD_POSITION.BOTTOM_CENTER,
-//     isTesting: true, // TODO: remove this line when release
-//     overlap: false,
-//     offsetTopBar: false,
-//     bgColor: 'black'
-//   } );
+  // this will create a banner on startup
+  AdMob.createBanner( {
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    isTesting: true, // TODO: remove this line when release
+    overlap: false,
+    offsetTopBar: false,
+    bgColor: 'black'
+  } );
 
-//   // this will load a full screen ad on startup
-//   AdMob.prepareInterstitial({
-//     adId: admobid.interstitial,
-//     isTesting: true, // TODO: remove this line when release
-//     autoShow: true
-//   });
-// }
+  // this will load a full screen ad on startup
+  AdMob.prepareInterstitial({
+    adId: admobid.interstitial,
+    isTesting: true, // TODO: remove this line when release
+    autoShow: true
+  });
+}
 
-// if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
-//     document.addEventListener('deviceready', initApp, false);
-// } else {
-//     initApp();
-// }
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
